cfcac1bc9610e6f23859fbfee94b74b469a42eea
datasense <datasense.learning@gmail.com>
2025-08-30 23:18:41 -0700

    func

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b 
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 def my_func(a, b):
     pass
+=======
+=======
+>>>>>>> Stashed changes
+def test(a,b):
+    return a*b
+
+
+def slow(str):
+    return "Slow Down"
+
+def fast(str):
+    return "Fasten Up"
+
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
